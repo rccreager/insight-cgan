f37935838874bd19b03bcc4472df7b65351d5e3e
Rachael Creager <rccreager@gmail.com>
2019-08-18 15:45:27 +0000

    first commit

---
+++ CGAN.py
@@ -0,0 +1,135 @@
+import argparse
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+
+####################################################
+
+seed = 42
+np.random.seed(seed)
+tf.compat.v1.set_random_seed(seed)
+
+Z_dim = 100
+img_size = 784 
+batch_size = 128
+
+####################################################
+
+def norm_init(size):
+    in_dim = size[0]
+    norm_stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.random.normal(shape=size, stddev=norm_stddev)
+
+#weights and biases for each discriminator layer
+D_W1 = tf.Variable(norm_init([img_size, batch_size]))
+D_b1 = tf.Variable(tf.zeros(shape=[batch_size]))
+D_W2 = tf.Variable(norm_init([batch_size, 1]))
+D_b2 = tf.Variable(tf.zeros(shape=[1]))
+theta_D = [D_W1, D_W2, D_b1, D_b2]
+#input to your discrim -- i.e. real data or output from generator
+X = tf.compat.v1.placeholder(tf.float32, shape=[None, 784], name='X')
+
+#weights and biases for each generator layer
+G_W1 = tf.Variable(norm_init([Z_dim, batch_size]))
+G_b1 = tf.Variable(tf.zeros(shape=[batch_size]))
+G_W2 = tf.Variable(norm_init([batch_size, img_size]))
+G_b2 = tf.Variable(tf.zeros(shape=[img_size]))
+theta_G = [G_W1, G_W2, G_b1, G_b2]
+#input to your generator, sampled from some prior
+Z = tf.compat.v1.placeholder(tf.float32, shape=[None, Z_dim])
+
+def generator(z):
+    G_h1 = tf.nn.relu(tf.matmul(z, G_W1) + G_b1)
+    G_log_prob = tf.matmul(G_h1, G_W2) + G_b2
+    G_prob = tf.nn.sigmoid(G_log_prob)
+    return G_prob
+
+def sample_Z(m, n):
+    return np.random.uniform(-1., 1., size=[m, n])
+
+def discriminator(x):
+    D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
+    D_logit = tf.matmul(D_h1, D_W2) + D_b2
+    D_prob = tf.nn.sigmoid(D_logit)
+    return D_prob, D_logit
+
+def plot(samples):
+    fig = plt.figure(figsize=(4, 4))
+    gs = gridspec.GridSpec(4, 4)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+
+    return fig
+
+def next_batch(num, data, labels):
+    '''
+    Return a total of `num` random samples and labels. 
+    '''
+    idx = np.arange(0 , len(data))
+    np.random.shuffle(idx)
+    idx = idx[:num]
+    data_shuffle = [data[ i] for i in idx]
+    labels_shuffle = [labels[ i] for i in idx]
+
+    return np.asarray(data_shuffle), np.asarray(labels_shuffle)
+
+####################################################
+
+G_sample = generator(Z)
+D_real, D_logit_real = discriminator(X)
+D_fake, D_logit_fake = discriminator(G_sample)
+D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_real, labels=tf.ones_like(D_logit_real)))
+D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.zeros_like(D_logit_fake)))
+D_loss = D_loss_real + D_loss_fake
+G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.ones_like(D_logit_fake)))
+
+D_solver = tf.compat.v1.train.AdamOptimizer().minimize(D_loss, var_list=theta_D)
+G_solver = tf.compat.v1.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
+
+####################################################
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], -1)
+train_filter = np.where((y_train == 8))
+x_train, y_train = x_train[train_filter], y_train[train_filter]
+x_train = x_train.astype('float32')
+x_train /= 255
+
+sess = tf.compat.v1.Session()
+sess.run(tf.global_variables_initializer())
+
+if not os.path.exists('out/'):
+    os.makedirs('out/')
+
+i = 0
+
+
+#for it in range(1000000):
+for it in range(100000):
+    if it % 1000 == 0:
+        samples = sess.run(G_sample, feed_dict={Z: sample_Z(16, Z_dim)})
+
+        fig = plot(samples)
+        plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+        i += 1
+        plt.close(fig)
+
+    X_mb, _ = next_batch(batch_size, x_train, y_train)
+
+    _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X: X_mb, Z: sample_Z(batch_size, Z_dim)})
+    _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: sample_Z(batch_size, Z_dim)})
+
+    if it % 1000 == 0:
+        print('Iter: {}'.format(it))
+        print('D loss: {:.4}'. format(D_loss_curr))
+        print('G_loss: {:.4}'.format(G_loss_curr))
---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install python-pip
+pip install numpy
+python CGAN.py 
+pip install tensorflow
+python CGAN.py 
+pip install matplotlib
+sudo apt-get install python-tk
+sudo apt install git-all
