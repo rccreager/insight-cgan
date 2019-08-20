2285bd7baa9261e3a7a6ef2005af07c89fd2e192
Rachael Creager <rccreager@gmail.com>
2019-08-20 14:50:46 +0000

    adding assume yes to apt get commands

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt install python3-pip
+sudo apt --assume-yes install python3-pip
 pip3 install numpy
 pip3 install tensorflow
 pip3 install matplotlib
-sudo apt-get install python-tk
+sudo apt-get --assume-yes install python-tk
